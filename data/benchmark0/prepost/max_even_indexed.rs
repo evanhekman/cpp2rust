ecad2166fef68b22ec4eae554238a80ad7d8e040
Evan Hekman <ehekman@umich.edu>
2026-04-08 13:06:50 -0400

    all 4 benchmarks?

--- data/benchmark0/prepost/max_even_indexed.rs
+++ data/benchmark0/prepost/max_even_indexed.rs
@@ -5,15 +5,14 @@ verus! {
 pub open spec fn max_even_indexed_spec(a: Seq<i32>) -> int
     decreases a.len()
 {
-    if a.len() <= 0 { -i32::MAX as int } else if a.len() == 1 { a[0] as int } else { let tail_max = max_even_indexed_spec(a.slice(2, a.len())); if a[0] as int > tail_max { a[0] as int } else { tail_max } }
+    if a.len() == 0 { i32::MIN as int } else if a.len() == 1 { a[0] as int } else { let max_rest = max_even_indexed_spec(a.subrange(2, a.len())); if a[0] as int > max_rest { a[0] as int } else { max_rest } }
 }
 
 pub fn max_even_indexed(a: &mut [i32]) -> (result: i32)
     requires
         a@.len() > 0,
-        a@.len() % 2 == 0,
     ensures
-        result as int == max_even_indexed_spec(a@),
+        result as int == max_even_indexed_spec(old(a)@),
 {
     assume(false);
 }
--- data/benchmark0/prepost/reverse.rs
+++ data/benchmark0/prepost/reverse.rs
@@ -6,7 +6,7 @@ pub fn reverse(a: &mut [i32]) -> (result: ())
     requires
 
     ensures
-        forall|i: int| 0 <= i && i < a@.len() ==> a@[i] == old(a@[(a@.len() - 1) - i]),
+        forall|i: int| 0 <= i && i < old(a)@.len() ==> a@[i] == old(a)@[old(a)@.len() - i - 1],
 {
     assume(false);
 }
